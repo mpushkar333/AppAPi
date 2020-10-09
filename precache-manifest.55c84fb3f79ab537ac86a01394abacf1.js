01f3a1abb759deaf4082589290bc26f80e25493b
mpushkar333 <mspushkar333@gmail.com>
2020-10-09 10:26:33 +0530

    First Commit

---
+++ precache-manifest.55c84fb3f79ab537ac86a01394abacf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7776f23e4b1c2dd994ce5f7b9023ec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2e8a53b2f6df5c3fca4",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "8350d666276509f62ddf",
+    "url": "/static/css/main.52047fbe.chunk.css"
+  },
+  {
+    "revision": "f2e8a53b2f6df5c3fca4",
+    "url": "/static/js/2.d7690547.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d7690547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8350d666276509f62ddf",
+    "url": "/static/js/main.037debc3.chunk.js"
+  },
+  {
+    "revision": "b957cec06c6437b7ad73",
+    "url": "/static/js/runtime-main.c7ceb169.js"
+  }
+]);
